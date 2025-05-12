ec715e3316bff8f8d1f1f5443fb63a7ed67edb77
Jeremias Wahlsten <jere.wahlsten@gmail.com>
2025-05-12 13:44:18 +0300

    removed every unused styling libary, added tailwindcss

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,56 +1,44 @@
 import React from 'react';
 import { Link as RouterLink } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
-import AppBar from '@mui/material/AppBar';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
-import { makeStyles } from '@mui/styles';
-
-const useStyles = makeStyles({
-  title: {
-    flexGrow: 1,
-  },
-  link: {
-    textDecoration: 'none',
-    color: 'inherit',
-  },
-});
 
 export default function Header() {
   const { i18n, t } = useTranslation();
-  const classes = useStyles();
 
   const changeLanguage = (lng: string) => {
     i18n.changeLanguage(lng);
   };
 
   return (
-    <AppBar position="static">
-      <Toolbar>
-        <div>
-          <Button color="inherit" onClick={() => changeLanguage('en')}>
+    <header className="bg-gray-800 text-white">
+      <div className="container mx-auto flex items-center justify-between py-4 px-6">
+        <div className="flex space-x-4">
+          <button
+            className="text-white hover:text-gray-300"
+            onClick={() => changeLanguage('en')}
+          >
             en
-          </Button>
-          <Button color="inherit" onClick={() => changeLanguage('fi')}>
+          </button>
+          <button
+            className="text-white hover:text-gray-300"
+            onClick={() => changeLanguage('fi')}
+          >
             fi
-          </Button>
+          </button>
         </div>
-        <Typography variant="h6" className={classes.title}>
-          {t('header')}
-        </Typography>
-        <nav>
-          <RouterLink to="/" className={classes.link}>
-            <Button color="inherit">{t('home')}</Button>
+        <h1 className="text-xl font-bold">{t('header')}</h1>
+        <nav className="flex space-x-4">
+          <RouterLink to="/" className="text-white hover:text-gray-300">
+            {t('home')}
           </RouterLink>
-          <RouterLink to="/board" className={classes.link}>
-            <Button color="inherit">{t('board')}</Button>
+          <RouterLink to="/board" className="text-white hover:text-gray-300">
+            {t('board')}
           </RouterLink>
-          <RouterLink to="/login" className={classes.link}>
-            <Button color="inherit">{t('login')}</Button>
+          <RouterLink to="/login" className="text-white hover:text-gray-300">
+            {t('login')}
           </RouterLink>
         </nav>
-      </Toolbar>
-    </AppBar>
+      </div>
+    </header>
   );
 }
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,9 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import 'materialize-css/dist/css/materialize.min.css';
 import App from './App.tsx'
 
-// import i18n (needs to be bundled ;)) 
+// import i18n (needs to be bundled ;))
 import './i18n';
 
 createRoot(document.getElementById('root')!).render(
